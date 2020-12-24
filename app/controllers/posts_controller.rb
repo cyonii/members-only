2ceba9d49d2825d4abbbf2274c2be97d58146bec
CY Kalu <cy23@tutanota.com>
2020-12-24 21:44:47 +0100

    Add title field to Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,6 +75,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body, :title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_165640) do
+ActiveRecord::Schema.define(version: 2020_12_24_202928) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_165640) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "member_id", null: false
+    t.string "title"
   end
 
 end
