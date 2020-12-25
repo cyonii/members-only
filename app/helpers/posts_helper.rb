b699c5b7ee679f44d38f825ecc00b707f4d6c850
CY Kalu <cy23@tutanota.com>
2020-12-25 16:26:10 +0100

    Rename templates to be more descriptive

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,8 @@
 module PostsHelper
-  def member_profile
-    return 'devise/shared/member_card' if member_signed_in?
-
-    'devise/shared/auth'
-  end
-
   def gravatar(post)
     alt = post.member.username
     email = post.member.email
-    gravatar_image_tag(email, alt: alt, class: 'rounded-circle av-48') if member_signed_in?
+    gravatar_image_tag(email, alt: alt, class: 'rounded av-48') if member_signed_in?
   end
 
   def flash_message
@@ -24,12 +18,18 @@ module PostsHelper
     render 'posts/form_errors' if @post.errors.any?
   end
 
-  def members_list
-    render 'devise/shared/members' if member_signed_in? and @members
+  def member_profile_card
+    return 'devise/shared/member_profile_card' if member_signed_in?
+
+    'devise/shared/auth'
   end
 
-  def member_header
-    render 'posts/member_header' if member_signed_in?
+  def members_list
+    render 'devise/shared/members_list' if member_signed_in? and @members
+  end
+
+  def post_member_header
+    render 'posts/post_member_header' if member_signed_in?
   end
 
   def post_owner_actions(post)
