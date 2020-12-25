f0a14ea4a3ec18130c4bbd1f8f9fb4cb3b2aedcb
CY Kalu <cy23@tutanota.com>
2020-12-25 17:54:24 +0100

    Add Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, length: { minimum: 1, maximum: 80 }
   validates :body, length: { minimum: 1, maximum: 1200 }
   belongs_to :member
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_165640) do
+ActiveRecord::Schema.define(version: 2020_12_25_164454) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text", null: false
+    t.integer "post_id", null: false
+    t.integer "member_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_comments_on_member_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_165640) do
     t.index ["member_id"], name: "index_posts_on_member_id"
   end
 
+  add_foreign_key "comments", "members"
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "members"
 end
