321781d63c621b714cb4ac7a719690d3e70d1e5f
CY Kalu <cy23@tutanota.com>
2020-12-25 02:10:06 +0100

    Remove obsolete code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,17 +9,10 @@ class PostsController < ApplicationController
     @members = Member.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show; end
-
   # GET /posts/new
   def new
-    @post = current_member.posts.build
+    @post = Post.new
   end
-
-  # GET /posts/1/edit
-  def edit; end
 
   # POST /posts
   # POST /posts.json
@@ -28,41 +21,12 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    if current_member == @post.member
-      @post.destroy
-      respond_to do |format|
-        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    else
-      flash[:alert] = 'Invalid op'
-      redirect_to @post
     end
   end
 
