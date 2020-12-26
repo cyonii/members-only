743da9e01f4b2455412f68e93617c75e7d62dee1
CY Kalu <cy23@tutanota.com>
2020-12-26 13:42:40 +0100

    Add Comment controllers - add #create and #destroy actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,9 @@ class PostsController < ApplicationController
 
   # GET /posts/1
   # GET /posts/1.json
-  def show; end
+  def show
+    @comment = Comment.new
+  end
 
   # GET /posts/1/edit
   def edit; end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :members, controllers: { 'registrations': 'registrations' }
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: %i[create destroy]
+  end
 end
