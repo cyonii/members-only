c8c7c1bdc327b90d5c9546fb8e71f1dedb0fcff8
CY Kalu <cy23@tutanota.com>
2020-12-26 13:57:46 +0100

    Destroy Post's Comments before destroying Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,6 +56,8 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    Comment.where(post_id: @post.id).destroy_all
+
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
