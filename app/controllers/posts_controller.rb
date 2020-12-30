d21f4290848fb6f49a0ee48a42af9de6715bd5b6
CY Kalu <cy23@tutanota.com>
2020-12-30 01:58:50 +0100

    Destroy comments on Post delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,8 +56,6 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    Comment.where(post_id: @post.id).destroy_all
-
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
