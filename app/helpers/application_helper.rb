9b9983614f0ee425fbcf5442f6084c7f993c673b
CY Kalu <cy23@tutanota.com>
2021-01-10 13:18:20 +0100

    Refactor
    - move helpers from `posts_helper` to `application_helper`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,26 @@ module ApplicationHelper
   def left_column
     render 'layouts/left_column' if user_signed_in? and @forums
   end
+
+  def gravatar(user, extra_class = nil)
+    alt = user.username
+    email = user.email
+    css_class = 'rounded'
+    css_class += " #{extra_class}" if extra_class
+
+    gravatar_image_tag(email, alt: alt, class: css_class) if user_signed_in?
+  end
+
+  def flash_message
+    render 'posts/flash' if flash.present?
+  end
+
+  def flash_class
+    return 'success' if flash[:notice]
+    return 'danger' if flash[:alert]
+  end
+
+  def form_errors
+    render 'posts/form_errors' if @post.errors.any?
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,26 +1,4 @@
 module PostsHelper
-  def gravatar(user, extra_class = nil)
-    alt = user.username
-    email = user.email
-    css_class = 'rounded'
-    css_class += " #{extra_class}" if extra_class
-
-    gravatar_image_tag(email, alt: alt, class: css_class) if user_signed_in?
-  end
-
-  def flash_message
-    render 'posts/flash' if flash.present?
-  end
-
-  def flash_class
-    return 'success' if flash[:notice]
-    return 'danger' if flash[:alert]
-  end
-
-  def form_errors
-    render 'posts/form_errors' if @post.errors.any?
-  end
-
   def post_user_header
     render 'posts/post_user_header' if user_signed_in?
   end
