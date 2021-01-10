820d1107ca391f9e632d6eb489545d19ee3e955c
CY Kalu <cy23@tutanota.com>
2021-01-10 11:20:31 +0100

    Add forum resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { 'registrations': 'registrations' }
-  root 'posts#index'
-  resources :posts do
-    resources :comments, only: %i[create destroy]
+  root 'application#index'
+  resources :forums do
+    resources :posts do
+      resources :comments, only: %i[create destroy]
+    end
   end
 end
