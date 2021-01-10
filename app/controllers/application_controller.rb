1604f44d7d2e0f5e26ae797fce8eceffe84e4242
CY Kalu <cy23@tutanota.com>
2021-01-10 15:08:42 +0100

    Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :set_user_data
 
   def index
+    @forums = current_user.forums
+    @suggested_forums = Forum.where("user_id != #{current_user.id}").limit(6)
     render template: 'layouts/index'
   end
 
   private
 
   def set_user_data
-    @forums = current_user.forums if current_user
     @comments_received = current_user.comments_received
-    @suggested_forums = Forum.where("user_id != #{current_user.id}").limit(6)
   end
 end
